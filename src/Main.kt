452754789691949fa3b4f189594e09c25c41c6b0
franconil <franconil04@gmail.com>
2024-01-28 23:13:01 +0100

    fumada solo la primera clase XD

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,30 @@
-fun main() {
-    println("Hello World!")
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class Tarea(val descripcion: String, var estado: Estado = Estado.PENDIENTE) {
+    val id: Int = contadorIds++
+    var fechaRealizacion: LocalDateTime? = null
+
+    enum class Estado {
+        PENDIENTE, REALIZADA
+    }
+
+    fun marcarRealizada() {
+        if (estado == Estado.PENDIENTE) {
+            estado = Estado.REALIZADA
+            fechaRealizacion = LocalDateTime.now()
+        }
+    }
+
+    override fun toString(): String {
+        return "ID: $id - Descripción: $descripcion - Estado: $estado - Fecha: ${formatoFechaRealizacion()}"
+    }
+
+    private fun formatoFechaRealizacion(): String {
+        return fechaRealizacion?.format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")) ?: "N/A"
+    }
+
+    companion object {
+        private var contadorIds = 1
+    }
 }
